499d11dba2a2b26a28517eb4aacd431622889851
AltinaKasabaqi <altinakasabaqi@gmail.com>
2023-12-22 16:43:11 +0100

    test

---
+++ easyApply/src/main/java/com/project/easyApply/EasyApplyApplication.java
@@ -0,0 +1,13 @@
+package com.project.easyApply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyApplyApplication.class, args);
+	}
+
+}
---
+++ easyApply/src/test/java/com/project/easyApply/EasyApplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.easyApply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyApplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
