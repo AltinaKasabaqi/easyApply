f24df6b89c558d3573ca538e162308057359a79e
AltinaKasabaqi <altinakasabaqi@gmail.com>
2023-12-22 18:29:11 +0100

    Testimi i pare

--- easyApply/src/main/java/com/project/easyApply/EasyApplyApplication.java
+++ easyApply/src/main/java/com/project/easyApply/EasyApplyApplication.java
@@ -1,13 +1,26 @@
 package com.project.easyApply;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.awt.Desktop;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 @SpringBootApplication
+@Controller
 public class EasyApplyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyApplyApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(){
+		return "FirstPage.html";
 	}
 
 }
