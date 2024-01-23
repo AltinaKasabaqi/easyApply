6ffd54129369fe04613f42e68f597579a395c5e2
diellzaaa1 <diellzasalihu10@gmail.com>
2024-01-23 17:26:03 +0100

    id of the auth. user

--- easyApply/src/main/java/com/project/easyApply/departmentManager/service/DepartamentiServiceImpl.java
+++ easyApply/src/main/java/com/project/easyApply/departmentManager/service/DepartamentiServiceImpl.java
@@ -14,7 +14,7 @@ public class DepartamentiServiceImpl implements DepartamentiService{
 
     @Autowired
     private DepartamentiRepository departamentiRepository;
-
+//commit
     @Autowired
     private UserService userService;
 
